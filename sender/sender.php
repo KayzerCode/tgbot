e33bdd3e877f835c8ccff100387ff75987732c9d
Inforax <inforax@gmail.com>
2020-10-09 16:03:39 +0300

    cut all from parser legacy

--- sender/sender.php
+++ sender/sender.php
@@ -6,10 +6,7 @@ $amt = count($forNotif);
 
 # если объявлений больше чем 0 - рассылаем уведомления #
 foreach ($subscibers as $chat) {
-	$text = $value['0']['name'];
-	if ($value['0']['price'] != '') {
-		$text .= "\n<b>" . $value['0']['price'] . "</b>";
-	}
+	$text = 'Hello! Im sender bot!';
 	$text = urlencode($text);
 	fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat}&parse_mode=html&text={$text}&disable_web_page_preview=1", "r");
 }
