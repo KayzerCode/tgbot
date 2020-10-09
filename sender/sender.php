a765f43b6919e8b2ec81e82455c3280aa1d476c9
Inforax <inforax@gmail.com>
2020-10-09 16:05:22 +0300

    cut all from parser legacy

--- sender/sender.php
+++ sender/sender.php
@@ -1,12 +1,11 @@
 <?php
 require_once __DIR__ . '/config/config.php';
 
-# подсчитываем количество найденных объявления #
-$amt = count($forNotif);
+# текст объявления #
+$text = 'Hello! Im sender bot!';
+$text = urlencode($text);
 
-# если объявлений больше чем 0 - рассылаем уведомления #
+# рассылаем уведомления #
 foreach ($subscibers as $chat) {
-	$text = 'Hello! Im sender bot!';
-	$text = urlencode($text);
 	fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat}&parse_mode=html&text={$text}&disable_web_page_preview=1", "r");
 }
