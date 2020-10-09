5cbae5c3d036b0736930b5ba5406412eabc704cd
Inforax <inforax@gmail.com>
2020-10-09 15:43:38 +0300

    inti

---
+++ sender/sender.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/config/config.php';
+
+# подсчитываем количество найденных объявления #
+$amt = count($forNotif);
+
+# если объявлений больше чем 0 - рассылаем уведомления #
+foreach ($subscibers as $chat) {
+	$text = $value['0']['name'];
+	if ($value['0']['price'] != '') {
+		$text .= "\n<b>" . $value['0']['price'] . "</b>";
+	}
+	$text = urlencode($text);
+	fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat}&parse_mode=html&text={$text}&disable_web_page_preview=1", "r");
+}
