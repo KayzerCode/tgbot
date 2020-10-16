c6a457df7b658fadb88d839f32d6d90ed9d53b2d
Kayzer Code <zlo.alien@gmail.com>
2020-10-16 22:56:33 +0300

    Added lib?

---
+++ reciever/bot.php
@@ -0,0 +1,16 @@
+<?php
+
+$body = file_get_contents('php://input'); 
+$arr = json_decode($body, true); 
+ 
+include_once ('telegramgclass.php');   
+
+$tg = new tg('1047235130:AAEDWRweu1okXr_6IhnaS2NYy0_xWrXcDyc');
+
+$chat_id = $arr['message']['chat']['id'];
+$userTgId = $arr['message']['from']['id'];
+$text = $arr['message']['text'];
+$coord1 = $arr['message']['location']['latitude'];
+$coord2 = $arr['message']['location']['longitude'];
+
+$tg->send($chat_id, "Нас не догонят!");
