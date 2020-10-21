9fbb584746b099667642f4921dd5e65216d72ba4
Kayzer Code <zlo.alien@gmail.com>
2020-10-21 13:25:21 +0300

    Menu are Buttons now"

--- reciever/bot.php
+++ reciever/bot.php
@@ -1,6 +1,6 @@
 <?php
 include('vendor/autoload.php');
-include('menu.php');
+include('buttons.php');
 require_once ('config/config.php');
 
 use Telegram\Bot\Api;
@@ -17,19 +17,23 @@ $l_name = $res['message']['from']['last_name'];
 
 
 if($text == "/start") {
-    $reply = "Выберете отчет: ";
-	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-	$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+    $reply = "Отчеты или настройки?";
+	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $btns_start, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);	
+}elseif($text == "Отчеты"){
+    $reply = "Какой выбираете отчет?";
+	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $btns_reports, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);	
 }elseif($text == "Отчет 1"){
     $reply = "Краткий или полный?";
-	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $menu2, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-	$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $btns_rep1, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);	
 }elseif($text == "Отчет 2"){
     $reply = "Краткий или полный?";
-	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $menu3, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-	$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $btns_rep2, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);	
 }elseif($text == "Назад"){
-    $reply = "Краткий или полный?";
-	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-	$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+    $reply = "Отчеты или настройки?";
+	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $btns_start, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);	
+}elseif($text == "Настройки"){
+    $reply = "Настройки?";
+	$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $btns_start, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);	
 } 
+
+$telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
---
+++ reciever/buttons.php
@@ -0,0 +1,17 @@
+<?
+# -====== EXAMPLE of format ======- #
+# rows
+# $btn_name = [["Name 1"],["Name 2"]];
+# cols
+# $btn_name = [["Name 1","Name 2"]];
+# -====== END Of EXAMPLE of format ======- #
+
+
+# Start screen
+$btns_start = [["Отчеты"],["Настройки"]];
+
+# Other buttons
+$btns_reports = [["Отчет 1"],["Отчет 2"]];
+$btns_rep1 = [["Отчет 1 Кратко","Отчет 1 Полный","Назад"]];
+$btns_rep2 = [["Отчет 2 Кратко","Отчет 2 Полный","Назад"]];
+?>
